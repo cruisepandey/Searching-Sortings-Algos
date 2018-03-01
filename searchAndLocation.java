3b5de14ee62d4563383c917a76a5999b5010f05f
cruisepandey <pandeyabhishek895@gmail.com>
2018-03-01 16:47:34 +0530

    Update searchAndLocation.java

--- searchAndLocation.java
+++ searchAndLocation.java
@@ -4,8 +4,8 @@ public class searchAndLocation {
 	
 	public searchAndLocation(boolean presence, int location){
 		if(presence!=false && location != 1){
-		System.out.println("Your number present or not - :" +presence );
-		System.out.println("Your desired number found at :"+location);
+			System.out.println("Your number present or not - :" +presence );
+			System.out.println("Your desired number found at :"+location);
 		}
 		else
 		{
